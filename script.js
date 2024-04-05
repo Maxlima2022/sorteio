aaf1562710e594eb45cab822e42d8621776f619c
Maxlima2022 <137231151+Maxlima2022@users.noreply.github.com>
2024-04-05 20:46:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function sortearNomes() {
-    var nomesDisponiveis = ["João", "Maria", "Radja", "Ana", "Pedro", "Mariana", "Carlos", "Laura", "Fernando", "Paula", "Rafael", "Camila"];
+    var nomesDisponiveis = ["João", "Maria", "José", "Ana", "Pedro", "Mariana", "Carlos", "Laura", "Fernando", "Paula", "Rafael", "Camila", "Luisa", "Daniel", "Tatiana", "Bruno", "Carolina", "Lucas"];
     var grupo = [];
     
-    // Garantir que haja pelo menos 5 nomes disponíveis
-    if (nomesDisponiveis.length < 5) {
+    // Garantir que haja pelo menos 9 nomes disponíveis
+    if (nomesDisponiveis.length < 9) {
         alert("Não há nomes suficientes para sortear!");
         return;
     }
     
-    // Sortear 5 nomes aleatórios sem repetição
-    while (grupo.length < 5) {
+    // Sortear 9 nomes aleatórios sem repetição
+    while (grupo.length < 9) {
         var indice = Math.floor(Math.random() * nomesDisponiveis.length);
         var nomeSorteado = nomesDisponiveis.splice(indice, 1)[0]; // Remover e obter o nome sorteado
         grupo.push(nomeSorteado);
